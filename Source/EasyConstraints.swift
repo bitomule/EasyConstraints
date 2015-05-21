1699672bacc723c764222b726558301b619c2271
David Collado Sela <bitomule@gmail.com>
2015-05-21 17:03:51 +0200

    small error with private methods

--- Source/EasyConstraints.swift
+++ Source/EasyConstraints.swift
@@ -27,73 +27,73 @@ infix operator *<>< {}
 infix operator <|> {}
 
 //TOP
-func ^ (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat)) -> NSLayoutConstraint{
+public func ^ (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat)) -> NSLayoutConstraint{
     return NSLayoutConstraint(item: view1, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: relatedTo.view2, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: relatedTo.constant)
 }
 
-func ^ (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat,relation:NSLayoutRelation)) -> NSLayoutConstraint{
+public func ^ (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat,relation:NSLayoutRelation)) -> NSLayoutConstraint{
     return NSLayoutConstraint(item: view1, attribute: NSLayoutAttribute.Top, relatedBy: relatedTo.relation, toItem: relatedTo.view2, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: relatedTo.constant)
 }
 
 //BOTTOM
-func *^ (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat)) -> NSLayoutConstraint{
+public func *^ (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat)) -> NSLayoutConstraint{
     return NSLayoutConstraint(item: view1, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: relatedTo.view2, attribute: NSLayoutAttribute.Bottom, multiplier: 1, constant: relatedTo.constant)
 }
 
-func *^ (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat,relation:NSLayoutRelation)) -> NSLayoutConstraint{
+public func *^ (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat,relation:NSLayoutRelation)) -> NSLayoutConstraint{
     return NSLayoutConstraint(item: view1, attribute: NSLayoutAttribute.Bottom, relatedBy: relatedTo.relation, toItem: relatedTo.view2, attribute: NSLayoutAttribute.Bottom, multiplier: 1, constant: relatedTo.constant)
 }
 
 //LEFT
-func << (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat)) -> NSLayoutConstraint{
+public func << (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat)) -> NSLayoutConstraint{
     return NSLayoutConstraint(item: view1, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: relatedTo.view2, attribute: NSLayoutAttribute.Leading, multiplier: 1, constant: relatedTo.constant)
 }
 
-func << (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat,relation:NSLayoutRelation)) -> NSLayoutConstraint{
+public func << (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat,relation:NSLayoutRelation)) -> NSLayoutConstraint{
     return NSLayoutConstraint(item: view1, attribute: NSLayoutAttribute.Leading, relatedBy: relatedTo.relation, toItem: relatedTo.view2, attribute: NSLayoutAttribute.Leading, multiplier: 1, constant: relatedTo.constant)
 }
 
 //RIGHT
-func >> (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat)) -> NSLayoutConstraint{
+public func >> (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat)) -> NSLayoutConstraint{
     return NSLayoutConstraint(item: view1, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: relatedTo.view2, attribute: NSLayoutAttribute.Trailing, multiplier: 1, constant: relatedTo.constant)
 }
 
-func >> (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat,relation:NSLayoutRelation)) -> NSLayoutConstraint{
+public func >> (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat,relation:NSLayoutRelation)) -> NSLayoutConstraint{
     return NSLayoutConstraint(item: view1, attribute: NSLayoutAttribute.Trailing, relatedBy: relatedTo.relation, toItem: relatedTo.view2, attribute: NSLayoutAttribute.Trailing, multiplier: 1, constant: relatedTo.constant)
 }
 
 //LEADING HORIZONTAL SPACE
-func ><> (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat)) -> NSLayoutConstraint{
+public func ><> (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat)) -> NSLayoutConstraint{
     return NSLayoutConstraint(item: view1, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: relatedTo.view2, attribute: NSLayoutAttribute.Trailing, multiplier: 1, constant: relatedTo.constant)
 }
 
-func ><> (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat,relation:NSLayoutRelation)) -> NSLayoutConstraint{
+public func ><> (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat,relation:NSLayoutRelation)) -> NSLayoutConstraint{
     return NSLayoutConstraint(item: view1, attribute: NSLayoutAttribute.Leading, relatedBy: relatedTo.relation, toItem: relatedTo.view2, attribute: NSLayoutAttribute.Trailing, multiplier: 1, constant: relatedTo.constant)
 }
 
 //TRAILING HORIZONTAL SPACE
-func <>< (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat)) -> NSLayoutConstraint{
+public func <>< (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat)) -> NSLayoutConstraint{
     return NSLayoutConstraint(item: view1, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: relatedTo.view2, attribute: NSLayoutAttribute.Leading, multiplier: 1, constant: relatedTo.constant)
 }
 
-func <>< (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat,relation:NSLayoutRelation)) -> NSLayoutConstraint{
+public func <>< (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat,relation:NSLayoutRelation)) -> NSLayoutConstraint{
     return NSLayoutConstraint(item: view1, attribute: NSLayoutAttribute.Trailing, relatedBy: relatedTo.relation, toItem: relatedTo.view2, attribute: NSLayoutAttribute.Leading, multiplier: 1, constant: relatedTo.constant)
 }
 
 //LEADING VERTICAL SPACE
-func *><> (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat)) -> NSLayoutConstraint{
+public func *><> (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat)) -> NSLayoutConstraint{
     return NSLayoutConstraint(item: view1, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: relatedTo.view2, attribute: NSLayoutAttribute.Bottom, multiplier: 1, constant: relatedTo.constant)
 }
 
-func *><> (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat,relation:NSLayoutRelation)) -> NSLayoutConstraint{
+public func *><> (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat,relation:NSLayoutRelation)) -> NSLayoutConstraint{
     return NSLayoutConstraint(item: view1, attribute: NSLayoutAttribute.Top, relatedBy: relatedTo.relation, toItem: relatedTo.view2, attribute: NSLayoutAttribute.Bottom, multiplier: 1, constant: relatedTo.constant)
 }
 
 //TRAILING VERTICAL SPACE
-func *<>< (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat)) -> NSLayoutConstraint{
+public func *<>< (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat)) -> NSLayoutConstraint{
     return NSLayoutConstraint(item: view1, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: relatedTo.view2, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: relatedTo.constant)
 }
 
-func *<>< (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat,relation:NSLayoutRelation)) -> NSLayoutConstraint{
+public func *<>< (view1:UIView,relatedTo:(view2:UIView,constant:CGFloat,relation:NSLayoutRelation)) -> NSLayoutConstraint{
     return NSLayoutConstraint(item: view1, attribute: NSLayoutAttribute.Bottom, relatedBy: relatedTo.relation, toItem: relatedTo.view2, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: relatedTo.constant)
 }
